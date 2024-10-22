4ee3ebd495cff9a470448d4e1c7d3111d3bcc297
b-kkoo <bmcow331@naver.com>
2024-10-22 19:11:31 +0900

    [Update] QuestManager.cs
    Q1_requirements 2, 3

--- kkujun_week2/Assets/Scripts/QuestManager.cs
+++ kkujun_week2/Assets/Scripts/QuestManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    // [구현사항 1] 정적 필드 정의
+    // [구현사항 1] 정적 필드 정의 (완료)
     private static QuestManager instance;
 
     // [구현사항 2] 정적 프로퍼티 정의
@@ -13,13 +13,32 @@ public class QuestManager : MonoBehaviour
     {
         get
         {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+            }
             return instance;
+        }
+        set
+        {
+            if ( instance == null )
+            {
+                GameObject instance = new GameObject("QuestManager");
+                instance.AddComponent<QuestManager>();
+            }            
         }
     }
 
     // [구현사항 3] 인스턴스 검사 로직
     private void Awake()
     {
-        instance = this;
+        if (instance == null)
+        {
+            instance = this;
+        }       
+        else
+        {
+            Destroy(this.gameObject);
+        }        
     }
 }
