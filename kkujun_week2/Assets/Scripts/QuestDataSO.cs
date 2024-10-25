71a1a9b0a4ae1a7f077e60528514f256485924ac
b-kkoo <bmcow331@naver.com>
2024-10-25 21:19:50 +0900

    [Add] ScriptableObject
    QuestDataSO, MonsterQuestDataSo, EncounterQuestDataSo 추가

---
+++ kkujun_week2/Assets/Scripts/QuestDataSO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class QuestDataSO : ScriptableObject
+{
+    public string QuestName;
+    public int QuestRequiredLevel;
+    public int QuestNPC;
+    public List<QuestDataSO> QuestPrerequisites;
+}
+
+[CreateAssetMenu]
+public class MonsterQuestDataSO : QuestDataSO
+{
+    public string QuestDetail;
+}
+
+[CreateAssetMenu]
+public class EncounterQuestDataSO : QuestDataSO
+{
+    public string QuestDetail;
+}
