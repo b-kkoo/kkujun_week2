2222f7d142bad06ce2fcae252691e812205ce1a5
b-kkoo <bmcow331@naver.com>
2024-10-25 12:21:29 +0900

    [Update] ObjectPool 구현
    Q2_requirement 1

--- kkujun_week2/Assets/Scripts/ObjectPool.cs
+++ kkujun_week2/Assets/Scripts/ObjectPool.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class ObjectPool : MonoBehaviour
 {
@@ -10,16 +11,34 @@ public class ObjectPool : MonoBehaviour
     void Start()
     {
         // 미리 poolSize만큼 게임오브젝트를 생성한다.
-
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.gameObject.SetActive(false);
+            pool.Add(obj);
+        }
     }
 
     public GameObject Get()
     {
-        // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
+        // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다
+        foreach (GameObject obj in pool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+
+        GameObject newObj = Instantiate(prefab);
+        pool.Add(newObj);
+        return newObj;
     }
 
     public void Release(GameObject obj)
     {
         // 게임오브젝트를 deactive한다.
+        obj.SetActive(false);
     }
 }
